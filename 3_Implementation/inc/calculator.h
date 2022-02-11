4c7f036e83230af7410739d7f8cddf97af6437b5
saicharanpatel <kottesaicharan@gmail.com>
2022-02-11 17:45:03 +0530

    main

---
+++ 3_Implementation/inc/calculator.h
@@ -0,0 +1,41 @@
+/**
+ * @file calculator.c
+ * @brief Mathematical Operations of calculator
+ * @version 0.1
+ * @date 2022-02-11
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include "calculator.h"
+
+//Header guards
+#ifndef CALCULATOR_H_
+#define CALCULATOR_H_
+
+//include files
+#include <stdio.h>  
+#include <math.h>  
+#include <stdlib.h>  
+
+// function declarations   
+int addition();  
+int subtract();  
+int multiply();  
+int divide();  
+int square();
+int sqrt();
+int cube(); 
+void exit();  
+  
+
+int addition()  
+int subtract() 
+int multiply() 
+int divide() 
+int square()
+int sqrt()
+int cube()
+
+#endif
